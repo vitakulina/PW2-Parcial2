d1a4a4397db5dfdc1b8a0d7a3c49a0dfb5b80982
vita-kulina <42756344+vita-kulina@users.noreply.github.com>
2020-11-02 21:58:34 -0300

    JUnit 5

---
+++ apiEcommerce/src/test/java/com/vitakulina/apiEcommerce/CartsTests.java
@@ -0,0 +1,36 @@
+package com.vitakulina.apiEcommerce;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vitakulina.apiEcommerce.repository.CartRepository;
+import com.vitakulina.apiEcommerce.service.CartService;
+import com.vitakulina.apiEcommerce.service.business.exception.CartException;
+
+
+@SpringBootTest
+public class CartsTests {
+	
+	@Autowired
+	private CartService cartService;
+	
+	@Autowired
+	private CartRepository cartRepo;
+	
+	@Test
+	@DisplayName("╯°□°）╯")
+	public void cartCheckoutWithInvalidIdThrowException() {
+		Assertions.assertThrows(CartException.class, () -> {
+			cartService.postCheckoutCart(77777L);
+		});
+		
+	}
+	
+	
+
+
+}
+
